3e9ca3acf25e162e2c93a6b956f6ec4be028551d
Bilal Alac <bilalalac@gmail.com>
2024-09-05 22:46:34 +0200

    formfield for add user

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDialogActions, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatDialogContent,
+    MatFormFieldModule,
+    MatDialogActions,
+    MatButtonModule,
+    MatDialogTitle,
+    MatInputModule,
+    MatDatepickerModule
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
+  ],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[1]);
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    this.dialog.open(DialogAddUserComponent);
+  }
+}
